01b4bcc0018f8e37bdbf64916491da0032ecf372
rohambob212 <rohambob2244@gmail.com>
2026-05-31 15:03:26 +0330

    lil changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import links as lnk
 import names as nms
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -30,27 +30,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(nms.stxt, reply_markup=reply_markup)
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
 
-    await query.answer()
-
-    await query.edit_message_text(text=f"Selected option: {query.data}")
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Use /start to test this bot.")
 
 
 def main() -> None:
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token("8430969457:AAHE-adi815_uNVU0QveFaWIXehQ0gdf1Hg").build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button))
-    application.add_handler(CommandHandler("help", help_command))
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
