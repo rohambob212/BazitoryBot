c5f7b3f681cfd74aae5964811bab07d011b889e2
rohambob212 <rohambob2244@gmail.com>
2026-05-31 18:32:37 +0330

    fixed bugs again

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import links as lnk
 import names as nms
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from scraper import getgames
+# from scraper import getgames
 from asyncio import create_task
-games : list = list(getgames())
+# games : list = list(getgames())
 tkn = "enter token here"
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -38,10 +38,10 @@ async def id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print(update.effective_chat.id)
 
 
-async def setgames(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    ngames = getgames()
-    if len(ngames) > len(games):
-        pass
+# async def setgames(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     ngames = getgames()
+#     if len(ngames) > len(games):
+#         pass
 
 def main() -> None:
     application = Application.builder().token(tkn).build()
