062c3deab056178588018d832bf08879e6b02625
rohambob212 <rohambob2244@gmail.com>
2026-06-02 12:07:10 +0330

    fixed BanDB, added logging

--- main.py
+++ main.py
@@ -87,6 +87,7 @@ async def banlistshow(update: Update, context: ContextTypes.DEFAULT_TYPE,pg: int
     rpg = pg
     pg *= amount
     dblen : int = len(db.keys)
+    print(dblen)
     if dblen == 0:
         await update.message.reply_text("شما هنوز کسی را بن نکرده اید")
         return
