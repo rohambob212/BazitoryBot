5fec51c9174f2dd065f527e20592db4d1a7cc1d9
rohambob212 <rohambob2244@gmail.com>
2026-06-02 12:58:19 +0330

    fixed banlist bugs

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ async def banlistshow(update: Update, context: ContextTypes.DEFAULT_TYPE,pg: int
     kb : list = []
     for key in list(db.keys())[(pg-amount):(pg)]:
         kb.append([InlineKeyboardButton(db[key]["name"], callback_data=("user"+key))])
-    kb.append([InlineKeyboardButton(f"📄 صفحه : {pg}/{allpgs}")])
+    kb.append([InlineKeyboardButton(f"📄 صفحه : {pg}/{allpgs}", callback_data="pg")])
     pgmovers : list[InlineKeyboardButton] = []
     if pg < allpgs:
         pgmovers.append(InlineKeyboardButton("صفحه بعد ⬅️", callback_data=f"gobanlistpg{pg-1}"))
