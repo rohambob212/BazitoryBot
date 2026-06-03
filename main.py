2681d5edd0a217e48e63438113720722e39425b4
rohambob212 <rohambob2244@gmail.com>
2026-06-03 17:53:13 +0330

    added debugging

--- main.py
+++ main.py
@@ -50,11 +50,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(nms.stxt, reply_markup=reply_markup)
 
 async def tag(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   print(update.effective_chat.type)
     if update.effective_chat.type == ChatType.PRIVATE:
         await update.message.reply_text("فقط توی گروه ها میتونی تگت رو عوض کنی")
         return
     await context.bot.set_chat_member_tag(update.effective_chat.id, update.message.from_user.id, update.message.text.replace("/tag ", ""))
     await update.message.reply_text(f"تگ شما به {update.message.text.replace("/tag ", "")} تغییر کرد")
+
 async def ban(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.effective_chat.type == ChatType.PRIVATE:
         await update.message.reply_text("فقط توی گروه ها میتونی بن کنی")
